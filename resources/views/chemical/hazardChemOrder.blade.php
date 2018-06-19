99273d3c8732ecf43c07ce323d3abab58c1f0e1c
yushaojun <hustysj@gmail.com>
2018-06-19 21:14:51 +0800

    no message

--- resources/views/chemical/hazardChemOrder.blade.php
+++ resources/views/chemical/hazardChemOrder.blade.php
@@ -2,21 +2,28 @@
     <form class="layui-form" id="orderForm" disabled="">
         <div class="layui-form-item">
             <div class="layui-inline">
-                <label class="layui-form-label">负责人姓名</label>
+                <label class="layui-form-label"> 负责人</label>
+                <div class="layui-input-inline">
+                    <input type="text" name="申购人姓名" lay-verify="required"
+                           class="layui-input" value="{{$order->fuzeren}}">
+                </div>
+            </div>
+            <div class="layui-inline">
+                <label class="layui-form-label">申购人姓名</label>
                 <div class="layui-input-inline">
                     <input type="text" name="申购人姓名" lay-verify="required"
                            class="layui-input" value="{{$order->申购人姓名}}">
                 </div>
             </div>
             <div class="layui-inline">
-                <label class="layui-form-label">手机号</label>
+                <label class="layui-form-label">申购人手机号</label>
                 <div class="layui-input-inline">
                     <input type="text" name="申购人手机号" lay-verify="required|phone"
                            class="layui-input" value="{{$order->申购人手机号}}">
                 </div>
             </div>
             <div class="layui-inline">
-                <label class="layui-form-label">身份证号</label>
+                <label class="layui-form-label">申购人身份证号</label>
                 <div class="layui-input-inline">
                     <input type="text" name="申购人身份证号" lay-verify="required|identity"
                            class="layui-input" value="{{$order->申购人身份证号}}">
