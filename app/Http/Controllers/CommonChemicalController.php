75e4d417c6dd55d1458992ad9389071971cb9858
yushaojun <hustysj@gmail.com>
2018-12-13 02:59:12 -0500

    no message

--- app/Http/Controllers/CommonChemicalController.php
+++ app/Http/Controllers/CommonChemicalController.php
@@ -216,7 +216,7 @@ class CommonChemicalController extends Controller
             $zipFile->addFile(iconv("UTF-8", "GBK", $output2), '业务编号' . $batch->id . '-1-单价1000以上（含）.docx');
         }
         $zipFile->close();
-        return stringValue(file_exists($output));
+        return (string)(file_exists($output));
         return \response()->download($zip);
     }
 
